cafc2552f6e6b060baabf9543d2505a0758b6c00
atikxcode <liadbd20@gmail.com>
2024-08-15 16:59:43 +0600

    started dowing right side card design 1.33

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = { firstName: 'Atiqul' }
+  const loggedIn = {
+    firstName: 'Atiqul',
+    lastName: 'Islam',
+    email: 'atiqul.islam0108@gmail.com',
+  }
   return (
     <section className="home">
       <div className="home-content">
@@ -21,7 +26,10 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTION
       </div>
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
     </section>
   )
 }
---
+++ components/BankCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BankCard = () => {
+  return (
+    <div>
+      <h2>Bank Card</h2>
+    </div>
+  )
+}
+
+export default BankCard
