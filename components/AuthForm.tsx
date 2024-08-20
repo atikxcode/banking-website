b62642f315f3358de08d33373a36f5dde0df2149
atikxcode <liadbd20@gmail.com>
2024-08-21 03:08:16 +0600

    finished the sign in page functionality

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -23,7 +23,7 @@ import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
-import { signIn, signUp } from '@/lib/actions/user.actions'
+import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions'
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter()
@@ -58,11 +58,11 @@ const AuthForm = ({ type }: { type: string }) => {
       }
 
       if (type === 'sign-in') {
-        // const response = await signIn({
-        //   email: data.email,
-        //   password: data.password,
-        // })
-        // if (response) router.push('/')
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        })
+        if (response) router.push('/')
       }
     } catch (error) {
       console.log(error)
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,9 +6,12 @@ import { cookies } from "next/headers"
 import { parseStringify } from "../utils"
 
 
-export const signIn = async () => {
+export const signIn = async ({email, password}: signInProps) => {
   try {
-    // slkdgnsdkjb
+    const { account } = await createAdminClient();
+
+    const response = await account.createEmailPasswordSession(email, password)
+    return parseStringify(response);
   } catch (error) {
     console.error('Error', error)
   }
@@ -44,7 +47,8 @@ export const signUp = async (userData: SignUpParams) => {
 export async function getLoggedInUser() {
   try {
     const { account } = await createSessionClient();
-    return await account.get();
+    const user =  await account.get();
+    return parseStringify(user);
   } catch (error) {
     return null;
   }
