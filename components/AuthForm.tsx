139cf49392a8f68b5e47f9edc2b3f6b8f60f0f05
atikxcode <liadbd20@gmail.com>
2024-08-20 19:13:59 +0600

    user data fetching

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,8 +22,10 @@ import { Input } from '@/components/ui/input'
 import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter()
   const [user, setUser] = useState(null)
 
   const [isLoading, setIsLoading] = useState(false)
@@ -40,15 +42,32 @@ const AuthForm = ({ type }: { type: string }) => {
   })
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
 
     setIsLoading(true)
 
-    console.log(values)
+    try {
+      // Sign up with appwrite & create plaid token
 
-    setIsLoading(false)
+      if (type === 'sign-up') {
+        // const newUser = await signUp(data);
+        // setUser(newUser)
+      }
+
+      if (type === 'sign-in') {
+        // const response = await signIn({
+        //   email: data.email,
+        //   password: data.password,
+        // })
+        // if (response) router.push('/')
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   return (
@@ -106,6 +125,13 @@ const AuthForm = ({ type }: { type: string }) => {
                     name="address1"
                     label="Address"
                     placeholder="Enter your specific address"
+                  />
+
+                  <CustomInput
+                    control={form.control}
+                    name="city"
+                    label="City"
+                    placeholder="Enter your city"
                   />
 
                   <div className="flex gap-4">
