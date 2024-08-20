d1bc72126cedff5447714b4f5e589a53962f49d0
atikxcode <liadbd20@gmail.com>
2024-08-21 02:05:40 +0600

    user actions updated

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -23,6 +23,7 @@ import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
+import { signIn, signUp } from '@/lib/actions/user.actions'
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter()
@@ -52,16 +53,16 @@ const AuthForm = ({ type }: { type: string }) => {
       // Sign up with appwrite & create plaid token
 
       if (type === 'sign-up') {
-        // const newUser = await signUp(data);
-        // setUser(newUser)
+        const newUser = await signUp(data)
+        setUser(newUser)
       }
 
       if (type === 'sign-in') {
-        // const response = await signIn({
-        //   email: data.email,
-        //   password: data.password,
-        // })
-        // if (response) router.push('/')
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        })
+        if (response) router.push('/')
       }
     } catch (error) {
       console.log(error)
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,32 @@
+'use server'
+
+import { createSessionClient } from "../appwrite"
+
+
+export const signIn = async () => {
+  try {
+    // slkdgnsdkjb
+  } catch (error) {
+    console.error('Error', error)
+  }
+}
+
+
+export const signUp = async (userData: SignUpParams) => {
+  try {
+    // slkdgnsdkjb
+  } catch (error) {
+    console.error('Error', error)
+  }
+}
+
+
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    return null;
+  }
+}
